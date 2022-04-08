e491cdf1eb9b6a6b3394de2b7c8a5d9b50ea8720
g21emmka <g21emmka@student.his.se>
2022-04-08 14:20:39 +0200

    Seed code

---
+++ app/src/main/java/com/example/egendatatyp/Berg.java
@@ -0,0 +1,13 @@
+package com.example.egendatatyp;
+
+public class Berg {
+    private String name;
+    private String location;
+    private int height_m;
+
+    public Berg(String name, String location, int height_m) {
+        this.name = name;
+        this.location = location;
+        this.height_m = height_m;
+    }
+}
