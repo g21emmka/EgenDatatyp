36a74d6cc760ffa482d3d6c77cd3a5b6ae897a99
g21emmka <g21emmka@student.his.se>
2022-04-08 14:55:33 +0200

    Created Getters & Setters

--- app/src/main/java/com/example/egendatatyp/Berg.java
+++ app/src/main/java/com/example/egendatatyp/Berg.java
@@ -10,4 +10,28 @@ public class Berg {
         this.location = location;
         this.height_m = height_m;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getHeight_m() {
+        return height_m;
+    }
+
+    public void setHeight_m(int height_m) {
+        this.height_m = height_m;
+    }
 }
